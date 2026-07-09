f792f3aaddd2274ad480d0f67b1ca6dbcad4f003
Johan Mendez <johan.mendez@jikkosoft.com>
2026-07-09 08:00:02 -0500

    fix: remove unnecessary drop column statement in app_settings migration

--- src/db/migrations/0005_organic_luminals.sql
+++ src/db/migrations/0005_organic_luminals.sql
@@ -1,2 +1,1 @@
-ALTER TABLE `app_settings` ADD `calendar_simulation_occurrences` integer DEFAULT 24 NOT NULL;--> statement-breakpoint
-ALTER TABLE `app_settings` DROP COLUMN `calendar_simulation_months`;
+ALTER TABLE `app_settings` ADD `calendar_simulation_occurrences` integer DEFAULT 24 NOT NULL;
